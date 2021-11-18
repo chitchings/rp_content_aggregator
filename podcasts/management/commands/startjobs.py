32999b28d20ce64e4ab0d141de0bdba6ba3bfb8a
chitchings <chrishitchings@gmail.com>
2021-11-17 21:22:40 -0500

    completed project

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -1,11 +1,22 @@
-#podcasts/management/commands/startjobs.py
+# podcasts/management/commands/startjobs.py
 
+# from datetime import timezone
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
+import logging
 import feedparser
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
 from dateutil import parser
 
 from podcasts.models import Episode
+
+
+logger = logging.getLogger(__name__)
 
 
 def save_new_episodes(feed):
@@ -20,7 +31,7 @@ def save_new_episodes(feed):
     """
 
     podcasts_title = feed.channel.title
-    podcast_image = feed.channel.image['href']
+    podcast_image = feed.channel.image["href"]
 
     for item in feed.entries:
         if not Episode.objects.filter(guid=item.guid).exists():
@@ -35,15 +46,67 @@ def save_new_episodes(feed):
             )
             episode.save()
 
+
 def fetch_realpython_episodes():
     _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
     save_new_episodes(_feed)
+
 
 def fetch_talkpython_episodes():
     _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
     save_new_episodes(_feed)
 
+
+def fetch_timesuck_episodes():
+    _feed = feedparser.parse("https://feeds.simplecast.com/Llc7KL2K")
+    save_new_episodes(_feed)
+
+
+def delete_old_job_executions(max_age=604_800):
+    """Deletes all apscheduler job execution logs older than `max_age`."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
 class Command(BaseCommand):
-    def handle(self,*args,**options):
-        fetch_realpython_episodes()
-        fetch_talkpython_episodes()
+    help = "Runs apscheduler"
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            fetch_realpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="The Real Python Podcast",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: The Real Python Podcast")
+
+        scheduler.add_job(
+            fetch_realpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="Talk Python Feed",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: Talk Python Podcast")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(day_of_week="mon", hour="00", minute="00"),
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: Delete Old Job Executions.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
